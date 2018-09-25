5fa1b23094b5b1d4eafedb3f893d7aefed92b911
Ingrid Wang <ingridwa@>
2018-09-25 01:49:41 -0700

    react

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ingridwang.github.io
 this is a website
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import PageContainer from './PageContainer';
+
+ReactDOM.render(<PageContainer />, document.getElementById('page_container'));
