4e2e08ce4b91a973c8b3051737ff9ae5418c9fc1
Ingrid Wang <ingridwa@>
2018-09-25 02:36:17 -0700

    Updates

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PageContainer from './App';
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<PageContainer />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import PageContainer from './PageContainer';
-
-ReactDOM.render(<PageContainer />, document.getElementById('page_container'));
