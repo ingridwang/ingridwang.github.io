f0052afb79c92d065727849ea026981a62d1176c
Ingrid Wang <ingridwa@>
2018-09-25 12:32:47 -0700

    revert deploy

---
+++ src/Crafts.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Crafts extends React.Component {
+    render() {
+        return (
+        );
+    }
+}
+
+export default Crafts;
---
+++ src/Dance.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Dance extends React.Component {
+    render() {
+        return (
+        );
+    }
+}
+
+export default Dance;
