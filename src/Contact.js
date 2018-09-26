a506dc644d598c99fe0ff11760410f911abe5586
Ingrid Wang <ingridwa@>
2018-09-25 18:17:34 -0700

    CONTACT PAGE

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Contact.css";
 
 class Contact extends React.Component {
     render() {
         return (
-            <div>
+            <div className="contact">
+                <div className="contact-page-photo">
+                    <img id="andy" src={require('./img/contact.jpg')} alt="Contact-related content" />
+                </div>
+                <p>{"let's be friends"}</p>
                 <a href="https://www.instagram.com/bazingrid/" target="_blank" rel="noopener noreferrer">instagram</a>
                 <a href="https://github.com/ingridwang/" target="_blank" rel="noopener noreferrer">github</a>
                 <a href="https://www.linkedin.com/in/wangingrid/" target="_blank" rel="noopener noreferrer">linkedin</a>
