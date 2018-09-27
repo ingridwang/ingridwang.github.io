ea6324ad7d7ed507bd7950fc26db7544ccf82160
Ingrid Wang <ingridwa@>
2018-09-27 12:55:34 -0700

    hover skeleton

--- src/PageContainer.js
+++ src/PageContainer.js
@@ -17,43 +17,81 @@ import Contact from "./Contact";
 import "./index.css";
 import "./SideNavigation.css";
 
-const SideNavigation = () => (
-    <div id="sidenav">
-        <NavLink to="/" className="home-page-link">INGRID WANG</NavLink>
-        <div id="pages">
-            <NavLink to="/about" className="page-link">about</NavLink>
-            <NavLink to="/experience" className="page-link">experience</NavLink>
-            <NavLink to="/crafts" className="page-link">crafts</NavLink>
-            <NavLink to="/photography" className="page-link">photography</NavLink>
-            <NavLink to="/dance" className="page-link">dance</NavLink>
-            <NavLink to="/contact" className="page-link">contact</NavLink>
-        </div>
-    </div>
-);
+const hoverIndexNull = -1;
 
-const PageContainer = () => (
-    <Router>
-        <Route render={({location}) => (
-            <div>
-                <SideNavigation />
-                <div id="main">
-                    <TransitionGroup>
-                        <CSSTransition key={location.key} classNames="fade" timeout={500}>
-                            <Switch location={location}>
-                                <Route exact path="/" component={WordSearch}/>
-                                <Route path="/about" component={About}/>
-                                <Route path="/experience" component={Experience}/>
-                                <Route path="/crafts" component={Crafts}/>
-                                <Route path="/photography" component={Photography}/>
-                                <Route path="/dance" component={Dance}/>
-                                <Route path="/contact" component={Contact}/>
-                            </Switch>
-                        </CSSTransition>
-                    </TransitionGroup>
+class HoverPageLink extends React.Component {
+    mouseEnter = () => {
+        this.props.hoverEnterCallback(this.props.id);
+    }
+    mouseLeave = () => {
+        this.props.hoverExitCallback(this.props.id);
+    }
+    render() {
+        return (
+            <div onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave}>
+                <NavLink to={"/" + this.props.title} className="page-link">{this.props.title}</NavLink>
+            </div>
+        );
+    }
+}
+
+class PageContainer extends React.Component { 
+    constructor(props) {
+        super(props);
+        this.state = {
+            hoverIndex: hoverIndexNull
+        }
+    }
+
+    hoverEnter = (index) => {
+        this.setState({hoverIndex: index});
+        console.log("enter: " + index);
+    }
+
+    hoverExit = () => {
+        this.setState({hoverIndex: hoverIndexNull});
+        console.log("exit");
+    }
+
+    render() {
+        const SideNavigation = () => (
+            <div id="sidenav">
+                <NavLink to="/" className="home-page-link">INGRID WANG</NavLink>
+                <div id="pages">
+                    <HoverPageLink hoverEnterCallback={() => this.hoverEnter(0)} hoverExitCallback={this.hoverExit} title="about" />
+                    <HoverPageLink hoverEnterCallback={() => this.hoverEnter(1)} hoverExitCallback={this.hoverExit} title="experience" />
+                    <HoverPageLink hoverEnterCallback={() => this.hoverEnter(2)} hoverExitCallback={this.hoverExit} title="crafts" />
+                    <HoverPageLink hoverEnterCallback={() => this.hoverEnter(3)} hoverExitCallback={this.hoverExit} title="photography" />
+                    <HoverPageLink hoverEnterCallback={() => this.hoverEnter(4)} hoverExitCallback={this.hoverExit} title="dance" />
+                    <HoverPageLink hoverEnterCallback={() => this.hoverEnter(5)} hoverExitCallback={this.hoverExit} title="contact" />
                 </div>
             </div>
-        )}/>
-    </Router>
-);
+        );
+        return (
+            <Router>
+                <Route render={({location}) => (
+                    <div>
+                        <SideNavigation />
+                        <div id="main">
+                            <TransitionGroup>
+                                <CSSTransition key={location.key} classNames="fade" timeout={500}>
+                                    <Switch location={location}>
+                                        <Route exact path="/" component={() => <WordSearch hoverIndex={this.state.hoverIndex}/>}/>
+                                        <Route path="/about" component={About}/>
+                                        <Route path="/experience" component={Experience}/>
+                                        <Route path="/crafts" component={Crafts}/>
+                                        <Route path="/photography" component={Photography}/>
+                                        <Route path="/dance" component={Dance}/>
+                                        <Route path="/contact" component={Contact}/>
+                                    </Switch>
+                                </CSSTransition>
+                            </TransitionGroup>
+                        </div>
+                    </div>
+                )}/>
+            </Router>
+        );
+    }
+}
 
 export default PageContainer;
